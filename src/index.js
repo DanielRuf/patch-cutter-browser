cbfd690c0845e5e03c1b827c746dfcedacd7d27a
Daniel Ruf <mac1@daniel-ruf.de>
2019-12-08 23:46:53 +0100

    Fix path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('./main.css');
 function init() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').then(registration => {
+            navigator.serviceWorker.register('/patch-cutter-browser/sw.js').then(registration => {
                 console.log('SW registered: ', registration);
             }).catch(registrationError => {
                 console.log('SW registration failed: ', registrationError);
