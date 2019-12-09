c859b71786b50578cb814398a927a4d44eff11b6
Daniel Ruf <mac1@daniel-ruf.de>
2019-12-09 22:00:40 +0100

    Create new build

--- dist/precache-manifest.2a21a5e7db75bfe96d30717f17f504e9.js
+++ dist/precache-manifest.2a21a5e7db75bfe96d30717f17f504e9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c7479abf7ce5ceacbd68b75db872516",
+    "revision": "ed3e23a8deab45809b0e6efbb165e1cb",
     "url": "index.html"
   },
   {
@@ -9,6 +9,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5fca0af051fff3585c04",
     "url": "main.js"
+  },
+  {
+    "url": "manifest.7f108bbec0b24a040d90970798b7b3bd.json"
   },
   {
     "revision": "6e1e47d706556eac8524f396e785d4bb",
